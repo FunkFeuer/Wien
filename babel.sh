1aa01031b421b55dbfea687bd737d598ee1ba480
Christian Tanzer <tanzer@swing.co.at>
2014-08-31 13:28:29 +0200

    Update, improve translations (L10N)

--- babel.sh
+++ babel.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-# Copyright (C) 2012 Mag. Christian Tanzer All rights reserved
+# Copyright (C) 2012-2014 Mag. Christian Tanzer All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # ****************************************************************************
-# This script is part of the FFM program.
+# This script is part of the FFW program.
 #
 # This script is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -32,33 +32,34 @@
 #--
 
 cmd=${1:?"Specify a command: extract | language | compile"}; shift
+export PYTHONPATH=$(pwd):~/CNDB:$PYTHONPATH
 
 default_langs="en,de"
-default_dirs="_FFM ."
+default_dirs="_FFW ."
 lib=$(dirname $(python -c 'from _TFL import sos; print sos.path.dirname (sos.__file__)'))
 
 case "$cmd" in
     "extract" )
         dirs=${1:-${default_dirs}}; shift
-        ( cd ${lib}; ./babel_extract.sh extract )
+        ( cd ${lib}; ./babel.sh extract )
         python ${lib}/_TFL/Babel.py extract                                          \
             -bugs_address        "tanzer@swing.co.at,ralf@runtux.com"         \
             -charset             utf-8                                        \
             -copyright_holder    "Mag. Christian Tanzer, Ralf Schlatterbeck"  \
             -global_config       ${lib}/_MOM/base_babel.cfg                   \
-            -project             "FFM"                                        \
+            -project             "FFW"                                        \
             -sort                                                             \
                 $dirs
         ;;
     "language" )
         langs=${1:-${default_langs}}; shift
         dirs=${1:-${default_dirs}}; shift
-        ( cd ${lib}; ./babel_extract.sh language "${langs}" )
+        ( cd ${lib}; ./babel.sh language "${langs}" )
         python ${lib}/_TFL/Babel.py language -languages "${langs}" -sort $dirs
         ;;
     "compile" )
         langs=${1:-${default_langs}}; shift
-        ${lib}/babel.sh compile ./Command.py "${langs}"
+        ${lib}/babel.sh compile ./Command.py "${langs}" "${PYTHONPATH}"
         ;;
     * )
         echo "Unknown command $cmd; use one of"
